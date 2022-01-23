5645271ab2338729548b2a951e08103d469750dd
Jevin Olano <jed.olano@gmail.com>
2022-01-22 19:49:54 -0800

    jevin test

--- App.js
+++ App.js
@@ -5,6 +5,7 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text>SpotifyGO</Text>
+      <Text>Jevin was here</Text>
       <StatusBar style="auto" />
     </View>
   );
