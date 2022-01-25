5fead3a089d484f09c4f5e1a7be8564e09cf8813
William Dai <45790024+william-dai@users.noreply.github.com>
2022-01-24 18:01:59 -0800

    Readded code I overrided by accident; merging search bar and bottom bar nagivation code; commenting out pull menu temporarily

--- App.js
+++ App.js
@@ -1,14 +1,12 @@
-import "./style.css"
-
-import react from "react";
 import SwipeUpDown from 'react-native-swipe-up-down'
-import { View, Text } from "react-native";
+import React from "react";
+import { StyleSheet, SafeAreaView, View, TouchableOpacity, Text, TextInput } from 'react-native';
 
 function App() {
-  
+  const[text, onChangeText] = React.useState("Search ...")
   return (
     <>
-    <SwipeUpDown	
+    {/* <SwipeUpDown	//THIS IS THE SWIPE UP LIST
 	      itemMini = {<li className = "buttonList"><button className = "barButton" id = "songListButton">List</button></li>} // Pass props component when collapsed
 	      itemFull = {<div className = "songListDown" id = "songList">
         <a href = "#">1</a>
@@ -27,51 +25,115 @@ function App() {
           backgroundColor: 'green',
           zIndex: 100,
         }} // style for swipe
-      />
-    <View style = {{
-      backgroundColor: 'white',
+      /> */}
+    <View style = {{ //THIS IS THE BACKGROUND OF THE APP
       flexDirection: 'column',
       flex: 1,
     }}>
+      <View style = {{ //THIS IS THE SEARCH BAR
+        backgroundColor: 'white',
+        top: 20,
+        left: 5,
+        borderWidth: 1,
+        borderRadius: 10,
+        width: "50%",
+        padding: 5,
+      }}>
+        <TextInput styles = {{
+            borderColor: "black",
+            width: "50%",
+            borderWidth: 1,
+            borderRadius: 10,
+            padding: 10,
+          }}
+            onChangeText = {onChangeText}
+            placeholder = "Search ..."
+        ></TextInput>
+      </View>
+
       <View style = {{
         position: 'absolute',
-        alignSelf: 'center',
-        backgroundColor: 'rgba(255,0,0,0)',
-        bottom: 0,
+        bottom: 30,
         width: '100%',
-        height: 100,
+        zIndex: 1
+      }}>
+        <View style = {{
+          justifyContent: 'space-evenly',
+          flexDirection: 'row',
+        }}>
+          <TouchableOpacity activeOpacity='0.8' style={[styles.button]}>
+            <Text style = {[styles.buttonText]}>Resync</Text>
+          </TouchableOpacity>
+          <TouchableOpacity activeOpacity='0.8' style={[styles.button]}></TouchableOpacity>
+          <View style = {{width: 90}}/>
+          <TouchableOpacity activeOpacity='0.8' style={[styles.button]}></TouchableOpacity>
+          <TouchableOpacity activeOpacity='0.8' style={[styles.button]}>
+          <Text style = {[styles.buttonText]}>Settings</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+      <View style = {{
+        position: 'absolute',
+        bottom: 0,
+        width: '100%'
       }}>
         <View style = {{
           position: 'relative',
           alignSelf: 'center',
-          background: 'linear-gradient(to top, rgba(50,50,50,255), rgba(255,0,0,0))',
+          background: 'linear-gradient(to top, rgba(40,40,40,255), rgba(255,0,0,0))',
           width: '100%',
           height: 30
         }} />
         <View style = {{
           position: 'relative',
           bottom: 0,
-          backgroundColor: 'rgba(50,50,50,255)',
+          backgroundColor: 'rgba(40,40,40,255)',
           width: '100%',
-          height: 100
+          height: 70
         }} />
       </View>
-      <View style = {{ 
+      <TouchableOpacity activeOpacity='0.8' style = {{
         position: 'absolute',
         alignSelf: 'center',
         background: 'linear-gradient(to top, rgba(35,35,35,255), rgba(60,60,60,255))',
         shadowOpacity: 1,
         shadowRadius: 10,
-        width: 100,
-        height: 100,
+        width: 85,
+        height: 85,
         borderRadius: 50,
         bottom: 20,
         zIndex: 10
-       }} />
+      }} >
+        <View style = {[styles.profileButtonInner]} />
+       </TouchableOpacity>
     </View>
     </>
   );
 }
 
-
 export default App;
+
+const styles = StyleSheet.create({
+  button: {
+    width: 65,
+    height: 65,
+    borderRadius: 10,
+    backgroundColor: 'rgba(45,45,45,255)',
+    shadowOpacity: 0.75,
+    shadowRadius: 5,
+  },
+  profileButtonInner: {
+    alignSelf: 'center',
+    bottom: -6,
+    backgroundColor: 'rgba(100,100,100,255)',
+    width: 75,
+    height: 75,
+    borderRadius: 50,
+  },
+  buttonText: {
+    position: 'relative',
+    top: 40,
+    alignSelf: 'center',
+    color: 'white',
+  },
+});
