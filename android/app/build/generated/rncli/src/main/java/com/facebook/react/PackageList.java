20c0a436270e9c85b227702bd53b5a9998b2ce98
Ritwik Aluri <raluri@ucsc.edu>
2022-01-31 18:02:41 -0800

    Revert "Merge branch 'database' into main"
    This reverts commit 5c48be15de1440f86fdb99def5627d55649cebd7, reversing
    changes made to 33d3aa2a2118c0b857318a25f42312ed2c41d85c.
    
    Reverting to pre-database merge

--- android/app/build/generated/rncli/src/main/java/com/facebook/react/PackageList.java
+++ android/app/build/generated/rncli/src/main/java/com/facebook/react/PackageList.java
@@ -11,8 +11,6 @@ import com.facebook.react.shell.MainReactPackage;
 import java.util.Arrays;
 import java.util.ArrayList;
 
-// @react-native-firebase/app
-import io.invertase.firebase.app.ReactNativeFirebaseAppPackage;
 // react-native-maps
 import com.airbnb.android.react.maps.MapsPackage;
 
@@ -60,7 +58,6 @@ public class PackageList {
   public ArrayList<ReactPackage> getPackages() {
     return new ArrayList<>(Arrays.<ReactPackage>asList(
       new MainReactPackage(mConfig),
-      new ReactNativeFirebaseAppPackage(),
       new MapsPackage()
     ));
   }
