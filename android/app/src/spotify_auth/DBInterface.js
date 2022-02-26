13964b8fceafb684d14c234c570cd2ea0d8d96ab
Ritwik Aluri <raluri@ucsc.edu>
2022-02-26 03:27:22 -0800

    I'M SORRY I KNOW I PROMISED CLEANER COMMENTS but stuff happened and NOW I'll be fixing and cleaning stuff

--- android/app/src/spotify_auth/DBInterface.js
+++ android/app/src/spotify_auth/DBInterface.js
@@ -12,7 +12,7 @@ class DBInterface {
     // Alternative to avoid querying for useless data is just query for all users in lat/long
     // range, THEN shuffle and provide first 100 users in a list after shuffling
     async getNearbyUsers(curUserID) {
-        //const curUserRef = ref(this.db, 'users/' + curUserID);
+        const curUserRef = ref(this.db, 'users/' + curUserID);
         const usersRef = ref(this.db, "users/");
         //console.log("Helloooooo " + usersRef);  // usersRef is an object!
         // Notes: query() can be called on a ref!
@@ -30,9 +30,89 @@ class DBInterface {
         let nearbyUsers5 = await get(query(usersRef, orderByChild("coordinates/latitude"), startAt(2), endAt(3)));
         return nearbyUsers5;*/
 
-        await get(query(usersRef, orderByKey()));
+        /*await get(query(usersRef, orderByKey()));
         let nearbyUsers5 = await get(query(usersRef, orderByChild("coordinates/latitude"), startAt(2), endAt(3)));
         return nearbyUsers5;
+*/
+        /*let latVal = await get(child(curUserRef, "coordinates/latitude"));
+        let longVal = await get(child(curUserRef, "coordinates/longitude"));
+        let latQuery, longQuery;
+        await get(query(usersRef, orderByChild("coordinates/latitude"), startAt(2), endAt(3))).then((latQueryX) => {
+            latQuery = latQueryX;
+            await get(query(usersRef, orderByChild("coordinates/longitude"), startAt(2), endAt(3))).then((longQueryX) => {
+                longQuery = longQueryX;
+            });
+        });*/
+
+        let latVal = await get(child(curUserRef, "coordinates/latitude"));
+        let longVal = await get(child(curUserRef, "coordinates/longitude"));
+        // Choosing to get by longitude; I decided that it would return a more consistent and lower
+        // amount of total data on average for each user than using latitude (imagine people at the
+        // poles vs the equator; equator people would end up taking in far more data due to
+        // population density; but with longitude, it's pretty uniform no matter where in the world
+        // the user is).
+        // Note: get() returns null if nothing fits the query
+        let longQueryResult = await get(query(usersRef, orderByChild("coordinates/longitude"), startAt(longVal.val() - 100), endAt(longVal.val() + 100)));
+        return longQueryResult;
+
+        // So, uh, according to this link:
+        // https://stackoverflow.com/questions/48037766/how-can-i-query-and-filter-firebase-realtime-database
+        // I can only sort by one value at a time. I can sort by latitude or longitude, but not both
+        // Pick one, get the data, filter out the rest clientside
+        //let latVal = await get(child(curUserRef, "coordinates/latitude"));
+        //let longVal = await get(child(curUserRef, "coordinates/longitude"));
+        //let latQuery = query(usersRef, orderByChild("coordinates"), startAt(latVal.val(), "coordinates/latitude"), endAt(100, "coordinates/longitude"));
+        //let longQueryResult = await get(query(latQuery /*, startAt(-200, "longitude"), endAt(100, "longitude")*/));
+
+        // THIS WORKS! I GET IT NOW!
+        // I can't use the orderByChild("longitude") on latQuery because that already included orderByChild("latitude")!!!
+        // I CAN'T STACK QUERIES! I CAN ONLY ADD THE CONSTRAINTS TO EXISTING QUERIES!
+        // So the question is, how do I filter by both latitude AND longitude in the same query without using
+        // a second orderBy function...?
+        /*let latQuery = query(usersRef, orderByChild("latitude"), startAt(latVal.val()));
+        let longQueryResult = await get(query(latQuery, endAt(100)));*/
+
+        //let longQueryResult = await get(latQuery /*orderByChild("longitude"), startAt(longVal.val()), endAt(100)*/); // This works fine, but I still need the longitude to stack
+        //let longQueryResult = await get(query(latQuery /*orderByChild("longitude"), startAt(longVal.val()), endAt(100)*/)); // This still allows me to return
+        //return longQueryResult;
+        //await get(query(usersRef, orderByKey()));
+        //return get(query(usersRef, orderByChild("latitude"), startAt("N", "music"), endAt("v", "music")));
+        //return get(query(usersRef, orderByChild("latitude"), startAt(0), endAt(100)));
+        //let q1 = get(query(usersRef, orderByChild("music"), startAt("N"), endAt("v")));  // This is fine, but doing second query not working
+        //await get(query(usersRef, orderByKey()));
+        //let q2 = get(query(q1, orderByChild("longitude"), startAt(100), endAt(300)));
+        //return q2;
+        /*return get(query(usersRef, orderByChild("music"), startAt("N"), endAt("v"))).then((q1) => {
+            return get(query(q1, orderByChild("longitude"), startAt(100), endAt(300))).then((output) => {
+                return output;
+            });
+        })*/
+        //let q1 = await get(query(usersRef, orderByChild("music"), startAt("N"), endAt("v")));
+        //let q2 = await get(query(usersRef, orderByChild("longitude"), startAt(100), endAt(300)));
+        //return q2;
+        //let latQuery, longQuery;
+        /*let filteredOutput = await get(query(usersRef, orderByChild("coordinates/latitude"), startAt(2), endAt(3))).then((latQuery) => {
+            filteredOutput = latQuery;
+            get(query(usersRef, orderByChild("coordinates/longitude"), startAt(2), endAt(3))).then((longQuery) => {
+                filteredOutput = longQuery;
+                return longQuery;
+            });
+        });
+        return filteredOutput;*/
+        //await get(query(usersRef, orderByChild("coordinates/longitude"), startAt(2), endAt(3)));
+
+        /*await get(query(usersRef, orderByChild("coordinates/latitude"), startAt(2), endAt(3))).then((latQueryX) => {
+            latQuery = latQueryX;
+            await get(query(usersRef, orderByChild("coordinates/longitude"), startAt(2), endAt(3))).then((longQueryX) => {
+                longQuery = longQueryX;
+            });
+        });*/
+        //let longQuery = await get(query(latQuery, orderByChild("coordinates/longitude"), startAt(2), endAt(3)));
+        //let longQuery = await get(query(latQuery, orderByChild("coordinates/longitude"), startAt(2), endAt(3)));
+        //return longQuery;
+        //return latQuery;
+        //get(query(latQuery, orderByChild("coordinates/longitude"), startAt(longVal - 1), endAt(longVal + 1))).then((output) => { console.log(output); });
+        //return longQuery;
 
         // This is the bare minimum code needed for this function to work as a test
         /*const usersRef = ref(this.db, "users/");
