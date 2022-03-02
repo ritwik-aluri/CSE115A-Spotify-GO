44328bcb44ee8de431e0e62e7fed4a4a299e0229
Ritwik Aluri <raluri@ucsc.edu>
2022-03-01 18:34:13 -0800

    Database structure has changed according to UI team requests and Spotify team's suggestions, and DBInterface now has more setter functions and getter functions in general

--- android/app/src/database/DBInterface.js
+++ android/app/src/database/DBInterface.js
@@ -17,55 +17,178 @@ class DBInterface {
         this.db = database;
     }
 
+    /////////////////
+    // DO NOT PUSH NEW DATABASE STRUCTURE CHANGES UNTIL SURE THAT GABE DOESN'T ALREADY RETURN
+    // EVERYTHING WE NEED!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    /////////////////
     initUser(userid, _name=PLACEHOLDER_STRING, _spotify_url=PLACEHOLDER_STRING,
              _premium=false, _sharing=false, _lat=PLACEHOLDER_VALUE,
-             _long=PLACEHOLDER_VALUE, _playback=PLACEHOLDER_VALUE) {
+             _long=PLACEHOLDER_VALUE, _playback=PLACEHOLDER_VALUE,
+             songObj=null,
+             _music_id=PLACEHOLDER_STRING, _music_url=PLACEHOLDER_STRING,
+             _music_name=PLACEHOLDER_STRING, _music_artist=PLACEHOLDER_STRING,
+             _music_art=PLACEHOLDER_VALUE) {
         const userRef = ref(this.db, 'users/' + userid);
-        // Do we want to add options to initialize with a song or whether they're
-        // sharing their location
-        // What's spotify_url and playback??? And do we really need them?
-        set(userRef, {
-            // For now the things I don't know are set to null
-            name: _name,  // Display name
-            music: PLACEHOLDER_STRING,  // Song being listened to
-            premium: _premium,  // Whether user has Spotify Premium
-            sharing: _sharing,  // Whether user is sharing location
-            coordinates: {
-                latitude: _lat,  // User's location: latitude
-                longitude: _long  // User's location: longitude
-            },
-            // Unsure about these two below, but adding them anyway
-            spotify_url: _spotify_url,  // User's Spotify account URL
-            playback: _playback  // Whether user is listening to music
-        });
-    }
-
-    updateSong(userid, songObj=null) {
-        const userRef = ref(this.db, 'users/' + userid);
-        if (songObj !== null) {
-            update(userRef, { "music": songObj });
+        if (songObj !== null) {  // A structured song object was NOT passed in
+            set(userRef, {
+                // Everything has a default value so the database structure is maintained
+                name: _name,  // User's display name
+                spotify_url: _spotify_url,  // User's Spotify account URL
+                playback: _playback,  // Whether user is currently listening to music
+                premium: _premium,  // Whether user has Spotify Premium
+                // When songObj is NOT null, it should match "music"'s format in the "else" statement
+                music: songObj,
+                sharing: _sharing,  // Whether user is sharing location
+                coordinates: {
+                    latitude: _lat,  // User's location: latitude
+                    longitude: _long  // User's location: longitude
+                },
+            });
+        }
+        else {  // A structured song object WAS passed in
+                // Everything is the same except "music"'s internal structure is handled manually
+            set(userRef, {
+                name: _name,
+                spotify_url: _spotify_url,
+                playback: _playback,
+                premium: _premium,
+                // When songObj IS null, it should follow this format:
+                music: {  // Object containing variety of song data
+                    song_id: _music_id,  // Song's ID on Spotify
+                    song_url: _music_url,  // Spotify URL to the music
+                    name: _music_name,  // Name of music on Spotify
+                    artist: _music_artist,  // Name of artist of music on Spotify
+                    art: _music_art  // Album art for this song on Spotify
+                },
+                sharing: _sharing,
+                coordinates: {
+                    latitude: _lat,
+                    longitude: _long
+                },
+            });
         }
     }
 
-    // Pass in null to _lat/_long to keep it the same as before
-    updateLocation(userid, _lat=null, _long=null) {
-        const coordinatesRef = ref(this.db, 'users/' + userid + '/coordinates');
-        if (_lat !== null) {
-            //coordinatesRef.child("latitude").setValue(_lat);
-            update(coordinatesRef, { "latitude": _lat });
+    // For all functions starting with "update", pass in null to a field to keep it the same as
+    // before.
+
+    updateDisplayName(userid, _name=null) {
+        const userRef = ref(this.db, 'users/' + userid);
+        if (_name !== null) {
+            update(userRef, { "name": _name });
         }
-        if (_long !== null) {
-            //coordinatesRef.child("longitude").setValue(_long);
-            update(coordinatesRef, { "longitude": _long });
+    }
+
+    updateAccountURL(userid, _url=null) {
+        const userRef = ref(this.db, 'users/' + userid);
+        if (_name !== null) {
+            update(userRef, { "spotify_url": _url });
         }
     }
 
     updatePlaybackState(userid, _playback=null) {
         const userRef = ref(this.db, 'users/' + userid);
         if (_playback !== null) {
-            //userRef.child("playback").setValue(_playback);
             update(userRef, { "playback": _playback });
         }
+    }
+
+    updatePremiumStatus(userid, _premium=null) {
+        const userRef = ref(this.db, 'users/' + userid);
+        if (_premium !== null) {
+            update(userRef, { "premium": _premium });
+        }
+    }
+
+    // Update song with a structured song object (as shown in initUser) OR with complete data for
+    // ALL of the fields that would create a complete song object
+    updateSong(userid, songObj=null, _id=null, _url=null, _name=null, _artist=null, _art=null) {
+        const userRef = ref(this.db, "users/" + userid);
+        const userMusicRef = ref(this.db, "users/" + userid + "/music");
+        if (songObj !== null) {
+            update(userRef, { "music": songObj });
+        }
+        else if (songObj === null &&
+                (_id !== null && _url !== null && _name !== null && _artist !== null &&
+                _art !== null)) {
+            update(userMusicRef, { "song_id": _id,
+                                   "song_url": _url,
+                                   "name": _name,
+                                   "artist": _artist,
+                                   "art": _art });
+        }
+    }
+
+    updateLocationShareStatus(userid, _sharing=null) {
+        const userRef = ref(this.db, 'users/' + userid);
+        if (_sharing !== null) {
+            update(userRef, { "sharing": _sharing });
+        }
+    }
+
+    // Latitude and longitude can be updated independently of each other in this function (you do
+    // not need to pass in values for both)
+    updateLocation(userid, _lat=null, _long=null) {
+        const coordinatesRef = ref(this.db, 'users/' + userid + '/coordinates');
+        if (_lat !== null) {
+            update(coordinatesRef, { "latitude": _lat });
+        }
+        if (_long !== null) {
+            update(coordinatesRef, { "longitude": _long });
+        }
+    }
+
+    // The functions all starting with "get" return data for a specific value for a specific user
+    // from the database EXCEPT getNearbyUsers(), which returns a list of data for multiple users.
+
+    async getDisplayName(userid) {
+        const userRef = ref(this.db, 'users/' + userid);
+        let name = await get(child(userRef, "name"));
+        return name.val();
+    }
+
+    async getAccountURL(userid) {
+        const userRef = ref(this.db, 'users/' + userid);
+        let spotify_url = await get(child(userRef, "spotify_url"));
+        return spotify_url.val();
+    }
+
+    async getPlaybackState(userid) {
+        const userRef = ref(this.db, 'users/' + userid);
+        let playback = await get(child(userRef, "playback"));
+        return playback.val();
+    }
+
+    async getPremiumStatus(userid) {
+        const userRef = ref(this.db, 'users/' + userid);
+        let premium = await get(child(userRef, "premium"));
+        return premium.val();
+    }
+
+    // Update song with a structured song object (as shown in initUser) OR with complete data for
+    // ALL of the fields that would create a complete song object
+    async getSong(userid) {
+        const userRef = ref(this.db, "users/" + userid);
+        let song = await get(child(userRef, "music"));
+        return song.val();
+    }
+
+    async getLocationShareStatus(userid) {
+        const userRef = ref(this.db, 'users/' + userid);
+        let sharing = await get(child(userRef, "sharing"));
+        return sharing.val();
+    }
+
+    // Unlike the other "get" functions, getLocation() returns an object containing the latitude and
+    // longitude of a user
+    async getLocation(userid) {
+        const coordinatesRef = ref(this.db, 'users/' + userid + '/coordinates');
+        let latVal = await get(child(coordinatesRef, "latitude"));
+        let longVal = await get(child(coordinatesRef, "longitude"));
+        return {
+                    latitude: latVal.val(),
+                    longitude: longVal.val()
+               };
     }
 
     // Get "nearby" users entails getting users within +/- 0.5 latitude/longitude of the current user
