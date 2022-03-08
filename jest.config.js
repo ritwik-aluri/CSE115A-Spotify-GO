1df0470bf7df34eac32ead968538f9e8ea39bd9e
Aaron Nguyen <anguy297@ucsc.edu>
2022-03-07 18:43:51 -0800

    Fixed import test issue (need mocks)

--- jest.config.js
+++ jest.config.js
@@ -3,7 +3,11 @@ module.exports = {
     "ts-jest": {
       isolatedModules: true,
     },
-  preset: 'jest-puppeteer',
+    "__DEV__": true,
+  preset: 'react-native',
   transform: {"\\.ts$": ['ts-jest']}
   },
+  transformIgnorePatterns: [
+    "node_modules/(?!(@react-native|react-native|react-native-vector-icons|react-native-webview|react-native-maps|firebase|react-native-get-location|firebase-encode|react)/)"
+  ],
 };
